8ab64a689eb0cf403222d78d877c959cfacd07a2
0nebyte <34482393+0nebyte@users.noreply.github.com>
2017-12-21 22:45:57 +0700

    coba framework

--- Apotik/login.Designer.cs
+++ Apotik/login.Designer.cs
@@ -28,129 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
             this.txt_username = new System.Windows.Forms.TextBox();
             this.txt_password = new System.Windows.Forms.TextBox();
-            this.btn_login = new System.Windows.Forms.Button();
+            this.metroButtonLogin = new MetroFramework.Controls.MetroButton();
+            this.metroButtonClose = new MetroFramework.Controls.MetroButton();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.lbl_date = new System.Windows.Forms.Label();
-            this.btn_cancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.metroStyleManager1 = new MetroFramework.Components.MetroStyleManager(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // txt_username
+            // 
+            this.txt_username.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txt_username.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_username.Font = new System.Drawing.Font("Arial Narrow", 12F);
+            this.txt_username.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txt_username.Location = new System.Drawing.Point(47, 114);
+            this.txt_username.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+            this.txt_username.MaxLength = 20;
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(226, 26);
+            this.txt_username.TabIndex = 2;
+            this.txt_username.Click += new System.EventHandler(this.txt_username_Click);
+            this.txt_username.MouseEnter += new System.EventHandler(this.txt_username_MouseEnter);
+            // 
+            // txt_password
+            // 
+            this.txt_password.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_password.Font = new System.Drawing.Font("Arial Narrow", 12F);
+            this.txt_password.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txt_password.Location = new System.Drawing.Point(47, 170);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.PasswordChar = '*';
+            this.txt_password.Size = new System.Drawing.Size(226, 26);
+            this.txt_password.TabIndex = 3;
+            this.txt_password.Click += new System.EventHandler(this.txt_password_Click);
+            // 
+            // metroButtonLogin
+            // 
+            this.metroButtonLogin.Location = new System.Drawing.Point(142, 252);
+            this.metroButtonLogin.Name = "metroButtonLogin";
+            this.metroButtonLogin.Size = new System.Drawing.Size(152, 39);
+            this.metroButtonLogin.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButtonLogin.TabIndex = 13;
+            this.metroButtonLogin.Text = "Login";
+            this.metroButtonLogin.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButtonLogin.Click += new System.EventHandler(this.metroButtonLogin_Click);
+            // 
+            // metroButtonClose
+            // 
+            this.metroButtonClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.metroButtonClose.Location = new System.Drawing.Point(-6, 252);
+            this.metroButtonClose.Name = "metroButtonClose";
+            this.metroButtonClose.Size = new System.Drawing.Size(150, 39);
+            this.metroButtonClose.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButtonClose.TabIndex = 14;
+            this.metroButtonClose.Text = "Cancel";
+            this.metroButtonClose.Click += new System.EventHandler(this.metroButtonClose_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Apotik.Properties.Resources.User_Interface_Password_icon;
+            this.pictureBox2.Location = new System.Drawing.Point(15, 170);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(27, 27);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 15;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Apotik.Properties.Resources.user_icon;
+            this.pictureBox1.Location = new System.Drawing.Point(14, 113);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(27, 27);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 120);
+            this.label1.Font = new System.Drawing.Font("Arial", 15F);
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(54, 51);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "USERNAME";
+            this.label1.Size = new System.Drawing.Size(176, 23);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "APLIKASI APOTIK";
             // 
-            // txt_username
+            // metroStyleManager1
             // 
-            this.txt_username.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_username.Location = new System.Drawing.Point(11, 136);
-            this.txt_username.Name = "txt_username";
-            this.txt_username.Size = new System.Drawing.Size(415, 20);
-            this.txt_username.TabIndex = 2;
-            // 
-            // txt_password
-            // 
-            this.txt_password.Location = new System.Drawing.Point(11, 183);
-            this.txt_password.Name = "txt_password";
-            this.txt_password.PasswordChar = '*';
-            this.txt_password.Size = new System.Drawing.Size(415, 20);
-            this.txt_password.TabIndex = 3;
-            // 
-            // btn_login
-            // 
-            this.btn_login.BackColor = System.Drawing.Color.Transparent;
-            this.btn_login.Location = new System.Drawing.Point(255, 225);
-            this.btn_login.Name = "btn_login";
-            this.btn_login.Size = new System.Drawing.Size(171, 23);
-            this.btn_login.TabIndex = 4;
-            this.btn_login.Text = "LOGIN";
-            this.btn_login.UseVisualStyleBackColor = false;
-            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(163, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 106);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 167);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 13);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "PASSWORD";
-            // 
-            // lbl_date
-            // 
-            this.lbl_date.AutoSize = true;
-            this.lbl_date.Location = new System.Drawing.Point(163, 264);
-            this.lbl_date.Name = "lbl_date";
-            this.lbl_date.Size = new System.Drawing.Size(0, 13);
-            this.lbl_date.TabIndex = 6;
-            // 
-            // btn_cancel
-            // 
-            this.btn_cancel.Location = new System.Drawing.Point(11, 225);
-            this.btn_cancel.Name = "btn_cancel";
-            this.btn_cancel.Size = new System.Drawing.Size(170, 23);
-            this.btn_cancel.TabIndex = 7;
-            this.btn_cancel.Text = "CANCEL";
-            this.btn_cancel.UseVisualStyleBackColor = true;
-            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            this.metroStyleManager1.Owner = null;
+            this.metroStyleManager1.Style = MetroFramework.MetroColorStyle.Green;
             // 
             // login
             // 
-            this.AcceptButton = this.btn_login;
+            this.AcceptButton = this.metroButtonLogin;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(438, 289);
-            this.Controls.Add(this.btn_cancel);
-            this.Controls.Add(this.lbl_date);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.btn_login);
-            this.Controls.Add(this.txt_password);
-            this.Controls.Add(this.txt_username);
-            this.Controls.Add(this.label2);
+            this.CancelButton = this.metroButtonClose;
+            this.ClientSize = new System.Drawing.Size(289, 291);
             this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.metroButtonClose);
+            this.Controls.Add(this.metroButtonLogin);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.txt_password);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "login";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "login";
+            this.ShowIcon = false;
             this.Load += new System.EventHandler(this.login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txt_username;
         private System.Windows.Forms.TextBox txt_password;
-        private System.Windows.Forms.Button btn_login;
+        private MetroFramework.Controls.MetroButton metroButtonLogin;
+        private MetroFramework.Controls.MetroButton metroButtonClose;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label lbl_date;
-        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private MetroFramework.Components.MetroStyleManager metroStyleManager1;
     }
 }
--- Apotik/login.cs
+++ Apotik/login.cs
@@ -10,7 +10,8 @@ using System.Windows.Forms;
 
 namespace Apotik
 {
-    public partial class login : Form
+    // public partial class login : Form
+    public partial class login : MetroFramework.Forms.MetroForm
     {
         private Controller controller;
 
@@ -38,10 +39,52 @@ namespace Apotik
 
         private void login_Load(object sender, EventArgs e)
         {
-            lbl_date.Text = DateTime.Now.ToString();
+         
+            
         }
 
         private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txt_username_Click(object sender, EventArgs e)
+        {
+            txt_username.Text = "";
+        }
+
+        private void txt_password_Click(object sender, EventArgs e)
+        {
+            txt_password.Text = "";
+            txt_password.PasswordChar = '*';
+        }
+
+        private void txt_username_MouseEnter(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void metroButtonLogin_Click(object sender, EventArgs e)
+        {
+            if (txt_username.Text == "admin" && txt_password.Text == "admin")
+            {
+                var form = new Home(controller);
+                form.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Username atau password salah!", "warning", MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+            }
+        }
+
+        private void metroButtonClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void metroTile1_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
